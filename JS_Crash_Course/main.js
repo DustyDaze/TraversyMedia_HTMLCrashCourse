eb8670bb129e1c8e6806ffab1d3c2e1ecdc8b7f8
DustyDaze <globalartichoke@googlemail.com>
2021-10-25 23:11:18 +0100

    Javascript crash course - initial checkin.

---
+++ JS_Crash_Course/main.js
@@ -0,0 +1,18 @@
+// Variables 
+// var - don't use as globally scoped
+// let, const
+
+// Datatypes
+// String, Numbers, Boolean, null, undefined
+
+const name = 'fred';
+const age = 22;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+//const z;
+
+console.log(typeof y);
+
+
